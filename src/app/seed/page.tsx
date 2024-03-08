00b7159e3fdd3abaa9d95a65a8c8eff2e874164f
candicecz <czech.candice@gmail.com>
2024-03-07 21:47:31 -0500

    feat: add admin authentication

--- src/app/seed/page.tsx
+++ src/app/seed/page.tsx
@@ -1,3 +1,4 @@
+import { getServerSession } from "next-auth/next";
 import { Dashboard } from "@/components/dashboard";
 import { Banner } from "./components/banner";
 import { SeedDatabase } from "./components/seed-btn";
@@ -6,8 +7,26 @@ import { seedCategories } from "./actions/categories";
 import { seedNominees } from "./actions/nominees";
 import { getCategories } from "@/queries/categories";
 import { getNominees } from "@/queries/nominees";
+import { authOptions } from "@/api/auth/[...nextauth]/config";
+import { getUserByEmail } from "@/queries/users";
 
 export default async function SeedDBPage() {
+  const session = await getServerSession(authOptions);
+  const email = session?.user?.email || "";
+  const { user } = await getUserByEmail({
+    email,
+  });
+
+  if (!user?.isAdmin)
+    return (
+      <>
+        <Banner />
+        <main className="flex flex-col mt-20 text-center text-red-500">
+          Sorry! Must be an admin to view this page.
+        </main>
+      </>
+    );
+
   const { categories } = await getCategories();
   const { nominees } = await getNominees();
   return (
@@ -18,18 +37,20 @@ export default async function SeedDBPage() {
           <Dashboard />
         </AuthRoute>
         {/* Order matters here, seed categories before nominees. */}
-        {/* <SeedDatabase
-          count={categories.length}
-          description="Seed categories before seeding nominees."
-          label="Seed Categories"
-          seedFn={seedCategories}
-        />
-        <SeedDatabase
-          count={nominees.length}
-          description="Seed nominees once categories are available."
-          label="Seed Nominees"
-          seedFn={seedNominees}
-        /> */}
+        <>
+          <SeedDatabase
+            count={categories.length}
+            description="Seed categories before seeding nominees."
+            label="Seed Categories"
+            seedFn={seedCategories}
+          />
+          <SeedDatabase
+            count={nominees.length}
+            description="Seed nominees once categories are available."
+            label="Seed Nominees"
+            seedFn={seedNominees}
+          />
+        </>
       </main>
     </>
   );
