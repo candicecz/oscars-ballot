cadb89a00a00f703bfd2a1114424be6fc0ade8c0
candicecz <czech.candice@gmail.com>
2024-03-10 20:06:18 -0400

    fix: revalidation

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,7 +1,9 @@
 import { getCategories } from "@/queries/categories";
 import { NextResponse } from "next/server";
+import { revalidatePath } from "next/cache";
 
 export async function GET(request: Request) {
   const categories = await getCategories();
+  revalidatePath("/");
   return NextResponse.json(categories, { status: 200 });
 }
